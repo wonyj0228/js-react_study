ca533db7c7c83d92559171b1c322016e9c3be0ed
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2023-10-09 21:23:49 +0900

    node.js module

---
+++ node/calc.js
@@ -0,0 +1,8 @@
+const add = (a, b) => a + b;
+const minus = (a, b) => a - b;
+
+module.exports = {
+  moduleName: 'calc module',
+  add: add,
+  minus: minus,
+};
---
+++ node/index.js
@@ -0,0 +1,5 @@
+const calc = require('./calc');
+
+console.log(calc.add(1, 2));
+console.log(calc.add(4, 5));
+console.log(calc.minus(10, 2));
